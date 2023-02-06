58846c3a502a59928f75c0d35596e87bcfffae89
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-06 18:57:13 +0500

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
+	$('[data-open-prices]').click(function(e) {
+		e.preventDefault();
+		$(this).find('span').toggle();
+		$('.railway__table-price-btn').trigger('click');
+	})
+
 	$('.railway__table-price-btn').click(function(e) {
 		e.preventDefault();
 		let ths = $(this);
@@ -17,16 +23,25 @@ $(function() {
 	$('[data-show-map]').click(function(e) {
 		e.preventDefault();
 		$('[data-map]').slideToggle(300);
+		$(this).find('span').toggle();
 	});
 
 	$('[data-copy]').click(function(e) {
 		e.preventDefault();
 		$(this).addClass('is-active');
+		$(this).find('span:last-child').show();
+		$(this).find('span:first-child').hide();
 		setTimeout(() => {
+			$(this).find('span:last-child').hide();
+			$(this).find('span:first-child').show();
 			$(this).removeClass('is-active');
-		}, 1000);
+		}, 2500);
 		let copyText = $(this).data('copy');
-		navigator.clipboard.writeText(copyText);
+		let $temp = $('<input>');
+		$('body').append($temp);
+		$temp.val(copyText).select();
+		document.execCommand('copy');
+		$temp.remove();
 	});
 
 });
