3f3a26d42d02bfcc137b3fbdd1c27dacd7e01b2c
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-06 14:36:44 +0500

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,21 @@ $(function() {
 			ths.find('.railway__table-price-text_show').show();
 			ths.find('.railway__table-price-text_hide').hide();
 		}
-	})
+	});
+
+	$('[data-show-map]').click(function(e) {
+		e.preventDefault();
+		$('[data-map]').slideToggle(300);
+	});
+
+	$('[data-copy]').click(function(e) {
+		e.preventDefault();
+		$(this).addClass('is-active');
+		setTimeout(() => {
+			$(this).removeClass('is-active');
+		}, 1000);
+		let copyText = $(this).data('copy');
+		navigator.clipboard.writeText(copyText);
+	});
 
 });
